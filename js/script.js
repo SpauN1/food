516857d4b31e72c1325b89981890672bdd839029
Spaun1 <andreispaun7@gmail.com>
2022-08-10 03:56:14 +0300

    added npm and gitignore

--- js/script.js
+++ js/script.js
@@ -261,14 +261,16 @@ window.addEventListener('DOMContentLoaded', function () {
 				},
 				body: JSON.stringify(object)
 			})
-			.then(data => data.text())
+			.then((data) => data.text())
 			.then((data) => {
 				console.log(data);
 				showThanksModal(message.success);
 				statusMessage.remove();
-			}).catch(() => {
+			})
+			.catch(() => {
 				showThanksModal(message.failure);
-			}).finally(() => {
+			})
+			.finally(() => {
 				form.reset();
 			});
 		});
@@ -296,4 +298,8 @@ window.addEventListener('DOMContentLoaded', function () {
 			closeModal();
 		}, 4000);
 	}
+
+	this.fetch('http://localhost:3000/menu')
+		.then((data) => data.json())
+		.then((res) => console.log(res));
 });
